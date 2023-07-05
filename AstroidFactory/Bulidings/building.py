b50d4196dcf823b93fea34646ed8c57fde4f89eb
adulescentia <adulescentia1124@gmail.com>
2023-07-05 23:11:08 +0900

    YEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE

--- AstroidFactory/Bulidings/building.py
+++ AstroidFactory/Bulidings/building.py
@@ -1,20 +1,50 @@
-class building:
-    size = 0
+#----------------------------------------------------------------
+#Frame
+
+class building: #건물
     price = 0
-    
-    positionX = 0
-    positionY = 0
+    Xpos = 0
+    Ypos = 0
     width = 0
     length = 0
 
 
-class 
+class productionBuilding(building): #기본 생산 건물
+    craftPeriod = 0 #제작 주기
+    output = [[]]
 
-class productionBuilding(building):
-    craftperiod = 0
-    output = 0
+class craftingBuilding(building):   #2차 생산 건물
+    craftPeriod = 0
+    input = [[]]
+    output = [[]]
 
-class craftingBuilding(building):
-    craftperiod = 0
-    input = 0
-    output = 0
+#----------------------------------------------------------------
+#builing
+
+#1차
+
+class mine(productionBuilding):
+    craftPeriod = 5
+    price = 100
+    width = 2
+    height = 2
+
+    def onwhich (ore):
+        #이 건물의 xpos, ypos에 겹치는 광석 있는지 확인
+        output = ["ore"[1]]
+
+class pump(productionBuilding):
+    craftperiod = 5
+    price = 100
+    width = 2
+    height = 2
+    output = ["water"[1]]
+
+#2차
+
+class refinery(craftingBuilding):
+    craftperiod = 10
+    price = 1000
+    width = 4
+    height = 4
+    input = ["water"[1], "ore"[2], "energy"[1]]
