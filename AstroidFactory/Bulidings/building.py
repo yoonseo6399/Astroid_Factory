acb9bcb9f71a771dcc16500628d9ab6a78003db4
yoonseo9057 <juhojuhorty@gmail.com>
2023-07-05 22:59:10 +0900

    init commit

---
+++ AstroidFactory/Bulidings/building.py
@@ -0,0 +1,20 @@
+class building:
+    size = 0
+    price = 0
+    
+    positionX = 0
+    positionY = 0
+    width = 0
+    length = 0
+
+
+class 
+
+class productionBuilding(building):
+    craftperiod = 0
+    output = 0
+
+class craftingBuilding(building):
+    craftperiod = 0
+    input = 0
+    output = 0
